1872ee12ffe846ab84fddccb1e56f5d3c471dd0f
nichitatrifan <nikiton254@gmail.com>
2022-01-30 18:15:29 -0600

    input fix

--- smtp.py
+++ smtp.py
@@ -6,12 +6,27 @@ import socket
 # gmail.com       MX preference = 10, mail exchanger = alt1.gmail-smtp-in.l.google.com
 
 if __name__ == '__main__':
-    MAIL_SERVER = str(input('Destination Server: '))
     PORT = 25
+    
+    MAIL_SERVER = str(input('Destination Server: '))
+    if not MAIL_SERVER:
+        MAIL_SERVER = 'gmail-smtp-in.l.google.com'
+    
     EMAIL_FROM = str(input('Email from: '))
+    if not EMAIL_FROM:
+        EMAIL_FROM = 'my_email@email.com'
+    
     RECPT_EMAIL = str(input('Recipient Email: '))
+    if not RECPT_EMAIL:
+        RECPT_EMAIL = 'nikiton254@gmail.com'
+    
     SUBJECT = str(input('Subject: '))
+    if not SUBJECT:
+        SUBJECT = 'Test'
+    
     BODY = str(input('Body: '))
+    if not BODY:
+        BODY = 'Text goest here!'
 
     commands = [
         f"HELO {MAIL_SERVER}\r\n",
